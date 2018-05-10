dd263f7de13743fe8c33360809c0ab76e37fc672
Ludivine Pellissier <ludivine.pellissier@gmail.com>
2018-05-10 15:15:36 +0200

    create route tracks + modif json

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
-const track1 = require("../mocks/tracks/1.json")
-const track2 = require("../mocks/tracks/2.json")
+const track1 = require('../mocks/tracks/1.json')
+const track2 = require('../mocks/tracks/2.json')
 
 const tracks = [track1, track2]
 
@@ -9,6 +9,10 @@ const app = express()
 
 app.get('/', (request, response) => {
   response.send('OK')
+})
+
+app.get('/tracks', (request, response) => {
+  response.json(tracks)
 })
 
 app.listen(3000, () => console.log(`J'écoute sur le port 3000`))
