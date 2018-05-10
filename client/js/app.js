5f4f4be2b5c94b4b933059e40b03e857b009660c
Ludivine Pellissier <ludivine.pellissier@gmail.com>
2018-05-10 15:48:57 +0200

    show mocks in client

--- client/js/app.js
+++ client/js/app.js
@@ -1,1 +1,7 @@
 fetch('http://localhost:3000/tracks')
+  .then(response => response.json())
+  .then(tracks => {
+    const tracksElement = document.getElementById('tracks')
+    tracksElement.innerHTML = JSON.stringify(tracks)
+    console.log(tracks)
+  })
